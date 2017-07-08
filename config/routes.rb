0c886329fe8fbdb63900d5a776f8f88b9822a437
Yannis Jaquet <yannisjaquet@mac.com>
2017-07-08 12:05:49 +0200

    Adds Rails Girls as scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :rails_girls
+
+  root to: 'rails_girls#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
